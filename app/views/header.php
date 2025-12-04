f46e7dbee0c154f9a7010e5ae78abc7023394f24
Ebenezer Quayson <2425401402@gctu.live.edu>
2025-12-04 03:24:29 +0000

    Tweaked the UI of the dashboard, made the table responsive. Also added a temporary timestap to the css link in header.php to prevent the browser from caching so that any changes in the css would be seen immediately.

--- app/views/header.php
+++ app/views/header.php
@@ -47,7 +47,7 @@ foreach ($settings as $setting) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/style.css?v=<?php echo time(); ?>">
     <style>
         :root {
             --primary-color: <?php echo $primary_color; ?>;
